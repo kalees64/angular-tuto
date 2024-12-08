e07153039b44de4eee8a200a3ace3781e0f41e4e
Kaleeswaran V <vkalees64@gmail.com>
2024-12-08 17:16:41 +0530

    chore: initial commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,35 @@
-import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { CommonModule } from '@angular/common';
+import { HttpClient } from '@angular/common/http';
+import { Component, OnInit } from '@angular/core';
+import { firstValueFrom } from 'rxjs';
+
+interface I_QUOTE {
+  id: number;
+  quote: string;
+  author: string;
+}
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [CommonModule],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrl: './app.component.css',
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'angular-tuto';
+
+  quotes!: I_QUOTE[];
+
+  constructor(private httpClient: HttpClient) {}
+
+  async getQuotes() {
+    const url = 'https://dummyjson.com/quotes';
+    const res: any = await firstValueFrom(this.httpClient.get(url));
+    console.log(res);
+    this.quotes = res.quotes;
+  }
+
+  ngOnInit(): void {
+    this.getQuotes();
+  }
 }
